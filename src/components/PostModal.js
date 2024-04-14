5056aac9bd5e8bffaa7f04fb5331d7b2739fa21e
dharmenderchoudhary99 <dharmenderchoudhary022@gmail.com>
2024-04-14 10:41:36 +0530

    Add UI elements to PostModal for user info, attach assets, and share

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -5,9 +5,33 @@ const PostModal = (props) => {
     <Container>
       <Content>
         <Header>
-            <h2>Create a post</h2>
-            <button><img src="/images/close-icon.png" alt="" /></button>
+          <h2>Create a post</h2>
+          <button>
+            <img src="/images/close-icon.png" alt="" />
+          </button>
         </Header>
+        <SharedContent>
+          <UserInfo>
+            <img src="/images/user.svg" alt="" />
+            <span>Name</span>
+          </UserInfo>
+        </SharedContent>
+        <SharedCreation>
+          <AttachAssets>
+            <AssetButton>
+              <img src="/images/share-image.png" alt="" />
+            </AssetButton>
+            <AssetButton>
+              <img src="/images/share-video.png" alt="" />
+            </AssetButton>
+          </AttachAssets>
+          <ShareComment>
+            <AssetButton>
+              <img src="/images/share-comment.png" alt="" />
+              Anyone
+            </AssetButton>
+          </ShareComment>
+        </SharedCreation>
       </Content>
     </Container>
   );
@@ -37,6 +61,89 @@ const Content = styled.div`
 `;
 
 const Header = styled.div`
+  display: block;
+  padding: 16px 20px;
+  border-bottom: 1ps solid rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    height: 40px;
+    width: 40px;
+    min-width: auto;
+    color: rgba(0, 0, 0, 0.15);
+    svg {
+      pointer-events: none;
+    }
+  }
+`;
+
+const SharedContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow-y: auto;
+  vertical-align: baseline;
+  background: transparent;
+  padding: 8px 12px;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 24px;
+  svg,
+  img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50%;
+  }
+  span {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    margin-left: 5px;
+  }
+`;
+
+const SharedCreation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 24px 12px 16px;
+`;
+
+const AttachAssets = styled.div`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  min-width: auto;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const AssetButton = styled.div`
+  align-items: center;
+  display: flex;
+  padding-right: 8px;
+  ${AssetButton} {
+    width: 40px;
+  }
+`;
+
+const ShareComment = styled.div`
+  padding-left: 8px;
+  margin-right: auto;
+  border-left: 1px solid rgba(0, 0, 0, 0.15);
+  ${AssetButton} {
+    svg {
+      margin-right: 5px;
+    }
+  }
 `;
 
 export default PostModal;
